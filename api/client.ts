8dbf7d7466d07d9d237824bea76846bffee32b10
Adebisi Joseph <adebisijoe@gmail.com>
2021-03-30 17:38:05 +0100

    added api sauce and apiclient

---
+++ api/client.ts
@@ -0,0 +1,9 @@
+import { create } from 'apisauce';
+const ACCESS_KEY='56353d59ca0e100b2e6273cc279fbd6c';
+
+// define the api
+const apiClient = create({
+  baseURL: `http://api.mediastack.com/v1/news?access_key = ${ACCESS_KEY}`,
+});
+
+export default apiClient;
